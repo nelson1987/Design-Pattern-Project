5748d40c6fa97ca82390312377f09f0fa8c68cb9
Nelson Neto <nelson.ash@outlook.com>
2020-04-17 11:57:53 -0300

    Criar Project Design Patterns - Repository

--- MyProj.Tests/Class1.cs
+++ MyProj.Tests/Class1.cs
@@ -1,8 +1,85 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace MyProj.Tests
 {
-    public class Class1
+    public class Produto
     {
+    }
+    public class ProdutoController
+    {
+        public ProdutoController(IProdutoRepository produtoRepository)
+        {
+            ProdutoRepository = produtoRepository;
+        }
+
+        private IProdutoRepository ProdutoRepository { get; set; }
+        public void Buscar()
+        {
+            ProdutoRepository.Buscar();
+        }
+
+    }
+
+    public interface IProdutoRepository : IMongoDbRepository<Produto>, IDapperSqlDbRepository<Produto>
+    {
+    }
+
+    public class ProdutoRepository : MongoDbRepository<Produto>
+    {
+    }
+
+    public interface IMongoDbRepository<TEntity>
+    {
+        IQueryable<TEntity> Buscar();
+    }
+
+    public class MongoDbRepository<TEntity> : IMongoDbRepository<TEntity> where TEntity : class
+    {
+        public IQueryable<TEntity> Buscar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IDapperSqlDbRepository<TEntity>
+    {
+        TEntity Inserir(TEntity entity);
+    }
+
+    public class DapperSqlDbRepository<TEntity> : IDapperSqlDbRepository<TEntity> where TEntity : class
+    {
+        public TEntity Inserir(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IEntityFrameworkSqlDbRepository<TEntity>
+    {
+        TEntity Inserir(TEntity entity);
+    }
+
+    public class EntityFrameworkSqlDbRepository<TEntity> : IEntityFrameworkSqlDbRepository<TEntity> where TEntity : class
+    {
+        public TEntity Inserir(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IXmlFileRepository<TEntity>
+    {
+        TEntity Ler(string linhaArquivo);
+
+    }
+
+    public class XmlFileRepository<TEntity> : IXmlFileRepository<TEntity> where TEntity : class
+    {
+        public TEntity Ler(string linhaArquivo)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
